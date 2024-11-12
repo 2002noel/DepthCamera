4de53d7074ee659cd130f73af257e966a433b999
Noel <kimn20@wwu.edu>
2024-11-12 15:25:48 -0800

    started the app

---
+++ DepthCamera/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  DepthCamera
+//
+//  Created by noel kim on 11/12/24.
+//
+
+import SwiftUI
+//depth camera
+import AVFoundation
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DepthCamera/DepthCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepthCameraApp.swift
+//  DepthCamera
+//
+//  Created by noel kim on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DepthCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
