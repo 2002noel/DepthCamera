203555da6007d02490e45dcfde5a61c57d11fe17
Noel <kimn20@wwu.edu>
2024-11-14 05:53:50 -0800

    Added in Camera View

---
+++ DepthCamera/FrameManager.swift
@@ -0,0 +1,8 @@
+//
+//  FrameManager.swift
+//  DepthCamera
+//
+//  Created by noel kim on 11/14/24.
+//
+
+import Foundation
---
+++ DepthCamera/FrameView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameView.swift
+//  DepthCamera
+//
+//  Created by noel kim on 11/14/24.
+//
+
+import SwiftUI
+
+struct FrameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameView()
+}
